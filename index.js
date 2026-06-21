85c9465033e9c78278c566a668997aa1f5e07a73
MaksumulEmon <maksumulemon300@gmail.com>
2026-06-21 21:38:30 +0600

    Initial commit: Verify Token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dontenv = require('dotenv')
 const cors = require("cors")
 const { MongoClient, ObjectId, ServerApiVersion, } = require('mongodb');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 dontenv.config()
 
 const uri = process.env.MONGODB_URI
@@ -19,7 +20,31 @@ const client = new MongoClient(uri, {
     }
 });
 
+const JKWS = createRemoteJWKSet(new URL(`${process.env.CLIENT_URL}/api/auth/jwks`))
 
+const verifyToken = async (req, res, next) => {
+    const authheader = req.headers.authorization;
+
+    console.log(authheader)
+
+    if (!authheader || !authheader.startsWith ("Bearer")) {
+        return res.status(401).json({ msg: "Unauthorized1" });
+    }
+
+    const token = authheader.split(" ")[1]
+    if (!token) {
+        return res.status(401).json({ msg: "Unauthorized2" });
+    }
+
+    try {
+        const { payload } = await jwtVerify(token, JKWS)
+        console.log(payload)
+        next()
+    } catch (error) {
+        console.log(error)
+        return res.status(401).json({ msg: "Unauthorized3" });
+    }
+}
 
 async function run() {
     try {
@@ -34,6 +59,32 @@ async function run() {
             const result = await bookCollection.find().toArray();
             res.json(result);
         });
+
+
+        app.get("/librarian/books", async (req, res) => {
+            const { userId } = req.query;
+
+            const result = await bookCollection.find({
+                userId: userId,
+                status: "published"
+            }).toArray();
+
+            res.json(result);
+        });
+
+
+        app.get("/librarian/books", async (req, res) => {
+            const { userId } = req.query;
+
+            const result = await bookCollection.find({
+                userId: userId
+            }).toArray();
+
+            res.send(result);
+        });
+
+
+
 
 
         // Manage invertry
@@ -104,7 +155,9 @@ async function run() {
 
 
         // Librain Add book
-        app.post("/librarian/books", async (req, res) => {
+        app.post("/librarian/books", verifyToken,  async (req, res) => {
+            const token = req.query.token
+            console.log(token)
             const books = req.body;
             const result = await bookCollection.insertOne(books);
             res.send(result);
@@ -126,7 +179,82 @@ async function run() {
 
 
 
+        app.get("/admin/pending-books", async (req, res) => {
+            try {
+                const result = await bookCollection.find({
+                    status: "pending"
+                }).toArray();
 
+                res.send(result);
+            } catch (error) {
+                res.status(500).send({ message: "Server Error" });
+            }
+        });
+
+
+        app.patch("/admin/book/approve/:id", async (req, res) => {
+            try {
+                const { id } = req.params;
+
+                const result = await bookCollection.updateOne(
+                    { _id: new ObjectId(id) },
+                    {
+                        $set: {
+                            status: "published",
+                            approvedAt: new Date()
+                        }
+                    }
+                );
+
+                res.send(result);
+
+            } catch (error) {
+                res.status(500).send({ message: "Approve failed" });
+            }
+        });
+
+
+
+
+        app.delete("/admin/book/:id", async (req, res) => {
+            try {
+                const { id } = req.params;
+
+                const result = await bookCollection.deleteOne({
+                    _id: new ObjectId(id)
+                });
+
+                res.send(result);
+
+            } catch (error) {
+                res.status(500).send({ message: "Delete failed" });
+            }
+        });
+
+
+
+        app.get("/admin/books", async (req, res) => {
+            try {
+                const result = await bookCollection.find().toArray();
+                res.send(result);
+            } catch (error) {
+                res.status(500).send({ message: "Failed to fetch books" });
+            }
+        });
+
+
+
+        app.get("/books", async (req, res) => {
+            try {
+                const result = await bookCollection.find({
+                    status: "published"
+                }).toArray();
+
+                res.send(result);
+            } catch (error) {
+                res.status(500).send({ message: "Failed" });
+            }
+        });
 
 
         await client.db("admin").command({ ping: 1 });
